05e99bb280269ba2f36d6364b5841d61ee8dcc43
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-08-28 17:30:25 +0530

    task can add using "Enter" key also

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import React, { useReducer, useState } from "react";
+import todoReducer from "./Reducer";
+
+const initialState = {
+    todos: [],
+    filter: "all"
+};
+
 function todoApp(){
     const [state,dispatch] = useReducer(todoReducer, initialState);
     const [inputValue, setInputValue] = useState("");
@@ -17,6 +25,11 @@ function todoApp(){
                     type="text"
                     value={inputValue}
                     onChange={(e) => setInputValue(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                            addToDo();
+                        }
+                    }}
                     placeholder="Add a new todo"
                 />
                 <button onClick={addToDo}>Add</button>
@@ -25,3 +38,5 @@ function todoApp(){
     )
 
 }
+
+export default todoApp;
